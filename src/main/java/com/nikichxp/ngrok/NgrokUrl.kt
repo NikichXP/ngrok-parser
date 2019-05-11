10aa781f4b4949064384b630bc8864d3aba7718e
Nikita <ruzhevsky@gmail.com>
2019-05-11 17:02:37 +0300

    Auto-start ngrok if command is present

--- src/main/java/com/nikichxp/ngrok/NgrokUrl.kt
+++ src/main/java/com/nikichxp/ngrok/NgrokUrl.kt
@@ -1,16 +1,61 @@
 package com.nikichxp.ngrok
 
 import com.mashape.unirest.http.Unirest
+import java.lang.Exception
 
-object NgrokUrl {
-    val url = Unirest
-            .get("http://localhost:4040/api/tunnels")
-            .asJson().body.`object`
-            .getJSONArray("tunnels")
-            .getJSONObject(0)
-            .get("public_url")
-            .toString()
-            .replace("http:", "https:")
+class NgrokUrl {
 
-//    fun getUrl() = url
+    private var executableLocation: String? = null
+    val ngrokAddress: String? by lazy {
+        tryLaunch()
+        loadNgrokAddr()
+    }
+    private var process: Process? = null
+
+    fun location(location: String) = apply {
+        this.executableLocation = location
+    }
+
+    private fun loadNgrokAddr(): String? = try {
+        Unirest.get("http://localhost:4040/api/tunnels")
+                .asJson().body.`object`
+                .getJSONArray("tunnels")
+                .getJSONObject(0)
+                .get("public_url")
+                .toString()
+                .replace("http:", "https:")
+    } catch (e: Exception) {
+        null
+    }
+
+    fun tryLaunch(): String? = executableLocation?.let {
+        if (loadNgrokAddr() == null) {
+            val builder = ProcessBuilder(
+                    if (it.startsWith('~')) {
+                        System.getProperty("user.home") + it.substring(1)
+                    } else {
+                        it
+                    }.let {
+                        if (it.endsWith('/')) {
+                            it + "/ngrok"
+                        } else {
+                            it
+                        }
+                    }, "http", "8080")
+            builder.redirectErrorStream(true)
+            process = builder.start()
+        }
+        (0..10).forEach {
+            val response = loadNgrokAddr()
+            if (response != null) {
+                return response
+            }
+            Thread.sleep(100)
+        }
+        return null
+    }
+
+    fun stopProcess() {
+        process?.destroy()
+    }
 }
